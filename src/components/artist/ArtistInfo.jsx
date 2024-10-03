6c0939bd9109c50d932c3e595abc39e84fd5a5fc
Kevin Nguyen <kevin.nguyen2321@yahoo.com>
2024-10-03 14:51:27 -0500

    Completed viewConcert component as well as ArtistInfo component

---
+++ src/components/artist/ArtistInfo.jsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from 'react';
+import './ArtistInfo.css';
+import { fetchRelatedArtists } from '../../services/artistServices';
+
+export const ArtistInfo = ({ isArtistModalOpen, artist, closeArtistModal }) => {
+  const [similarArtists, setSimilarArtists] = useState([]);
+
+  useEffect(() => {
+    if (artist.id) {
+      fetchRelatedArtists(artist.id).then((similarArtistsArr) => {
+        const firstFourSimilarArtistsArr = [];
+        for (let i = 0; i < 4; i++) {
+          const similarArtistObj = similarArtistsArr[i];
+          firstFourSimilarArtistsArr.push(similarArtistObj);
+        }
+        setSimilarArtists(firstFourSimilarArtistsArr);
+      });
+    }
+  }, [artist]);
+
+  const artistGenres =
+    artist && artist.genres
+      ? artist.genres
+          .map((genre) => genre.replace(/\b\w/g, (char) => char.toUpperCase()))
+          .join(', ')
+      : 'No genres available';
+
+  if (!isArtistModalOpen) {
+    return null;
+  }
+  return (
+    <div className="view-artist-modal-overlay" onClick={closeArtistModal}>
+      <div className="view-artist-modal-content">
+        <div className="artist-info-wrapper">
+          <button className="close-btn" onClick={closeArtistModal}>
+            X
+          </button>
+          <h2>{artist.name}</h2>
+          <img className="artist-info-pic" src={artist.images[1].url} />
+          <p>
+            <span>Genres:</span> {artistGenres}
+          </p>
+        </div>
+        <div className="similar-artist-wrapper">
+          <div className='similar-artist-header'>
+            <h2>Similar Artists</h2>
+          </div>
+
+          <div className="similar-artist-card-wrapper">
+            {similarArtists.map((similarArtist) => {
+              return (
+                <div className="similar-artist-card" key={similarArtist.id}>
+                  <h3>{similarArtist.name}</h3>
+                  <img src={similarArtist.images[2].url} />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/components/concerts/ViewConcert.jsx
+++ src/components/concerts/ViewConcert.jsx
@@ -2,10 +2,15 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getConcertByConcertId } from '../../services/concertServices';
 import './ViewConcert.css';
+import { fetchArtist } from '../../services/artistServices';
+import { ArtistInfo } from '../artist/ArtistInfo';
 
 export const ViewConcert = () => {
   const { concertId } = useParams();
   const [concert, setConcert] = useState({});
+  const [artist, setArtist] = useState({});
+  const [error, setError] = useState(null);
+  const [isArtistModalOpen, setIsArtistModalOpen] = useState(false);
 
   useEffect(() => {
     getConcertByConcertId(concertId).then((concertArr) => {
@@ -14,5 +19,90 @@ export const ViewConcert = () => {
     });
   }, [concertId]);
 
-  return <>heloo {concertId}</>;
+  useEffect(() => {
+    if (concert.artist) {
+      fetchArtist(concert.artist)
+        .then((artistArr) => {
+          if (artistArr.length > 0) {
+            const artistObj = artistArr[0];
+            setArtist(artistObj);
+          } else {
+            setError('Artist not found');
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+          setError('Failed to fetch artist');
+        });
+    }
+  }, [concert]);
+
+  //Function to format date to MM/DD/YYY format//
+  const formatDate = (dateStr) => {
+    const dateObj = new Date(dateStr);
+
+    const month = (dateObj.getMonth() + 1).toString().padStart(2, '0'); // getMonth() is zero-based
+    const day = dateObj.getDate().toString().padStart(2, '0');
+    const year = dateObj.getFullYear();
+
+    const formattedDate = `${month}/${day}/${year}`;
+    return formattedDate;
+  };
+
+  const handleOpenArtistModal = () => setIsArtistModalOpen(true);
+  const handleCloseArtistModal = () => setIsArtistModalOpen(false);
+
+  return (
+    <div className="concert-wrapper">
+      <h2>Concert Info</h2>
+      <div className="artist-image-wrapper">
+        {/* Check if artist images has loaded before rendering image */}
+        {artist.images && artist.images.length > 0 && (
+          <img
+            className="artist-image"
+            src={artist.images[0].url}
+            alt="Artist pic"
+            onClick={handleOpenArtistModal}
+          />
+        )}
+        {/* If there is an error fetching artist then use dummy image */}
+        {error && (
+          <img
+            className="artist-image"
+            src={`http://dummyimage.com/150x150/cccccc/ffffff&text=${concert.artist}`}
+          />
+        )}
+      </div>
+      <div>
+        <p>
+          <span>Artist:</span>
+          {concert.artist}
+        </p>
+      </div>
+
+      <div>
+        <p>
+          <span>Date:</span>
+          {formatDate(concert.date)}
+        </p>
+      </div>
+      <div>
+        <p>
+          <span>Venue:</span>
+          {concert.venue}
+        </p>
+      </div>
+      <div>
+        <p>
+          <span>Rating:</span>
+          {concert.rating}
+        </p>
+      </div>
+      <ArtistInfo
+        isArtistModalOpen={isArtistModalOpen}
+        closeArtistModal={handleCloseArtistModal}
+        artist={artist}
+      />
+    </div>
+  );
 };
